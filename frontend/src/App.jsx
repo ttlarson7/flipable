f6feb60808b392597120aacf47438e9eca0623ba
Peter Mora-Stevens <petermora-stevens@Peters-MacBook-Air.local>
2023-11-15 15:19:43 -0800

    added navbars element

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer"
+import Navbars from "./components/Navbars"
 import './index.css'
 
 const App = () => {
   return (
     <>
-      <div className="min-h-screen"></div>
+      <Navbars page={"landing"}></Navbars>
+      <div className="min-h-screen bg-primary-content"></div>
       <Footer></Footer>
     </>
   )
