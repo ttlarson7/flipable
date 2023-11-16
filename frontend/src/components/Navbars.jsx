b62f8217388a8765765f96081fb9beae3336e4ac
Taz Larson <tazlarson1@gmail.com>
2023-11-16 12:38:40 -0800

    design change for add button

--- frontend/src/components/Navbars.jsx
+++ frontend/src/components/Navbars.jsx
@@ -65,17 +65,16 @@ const Navbars = ({ page }) => {
             Quizify
           </Link>
           {/* Open the modal using document.getElementById('ID').showModal() method */}
-            <button className="btn glass text-black hover:text-white" onClick={()=>document.getElementById('my_modal_1').showModal()}><FaPlus />Add</button>
+            <button className="btn btn-ghost text-white hover:text-white" onClick={()=>document.getElementById('my_modal_1').showModal()}><FaPlus />Add</button>
             <dialog id="my_modal_1" className="modal">
               <div className="modal-box flex flex-col">
                 <h3 className="font-bold text-lg self-center">Add New Deck</h3>
-                <input value = {deckName} type="text" placeholder="Deck Name" className="input input-bordered input-primary w-full max-w-xs" onChange = {handleAddDeck}/>
-                <div className="modal-action ">
-                  <form method="dialog" className = "flex justify-between">
+                <input value = {deckName} type="text" placeholder="Deck Name" className="input input-bordered input-primary w-full max-w-xs self-center  m-10" onChange = {handleAddDeck}/>
+                <div className="modal-action flex">
+                  <form method="dialog" className = "flex justify-center w-full">
                     {/* if there is a button in form, it will close the modal */}
-                    <button className="btn" onClick = {handleClose} >Close</button>
-                    <button className="btn bg-transparent hover:btn-primary text-primary font-semibold hover:text-white border border-primary hover:border-transparent rounded-lg " onClick={handleAccept}>Add</button>
-                  
+                    <button className="btn mr-8 hover:btn-error text-error font-semibold hover:text-white border border-error hover:border-transparent rounded-lg" onClick = {handleClose} >Close</button>
+                    <button className="btn ml-8 bg-transparent hover:btn-primary text-primary font-semibold hover:text-white border border-primary hover:border-transparent rounded-lg " onClick={handleAccept}>Add</button>
                   </form>
                 </div>
               </div>
