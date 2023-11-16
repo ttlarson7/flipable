16ea513507816a7592bb911ad8512d361611076f
Peter Mora-Stevens <petermora-stevens@Peters-MacBook-Air.local>
2023-11-16 11:13:34 -0800

    added context and preparing for flashcards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,46 +7,56 @@ import {
   SignUp,
 } from "@clerk/clerk-react";
 import { Route, Routes, BrowserRouter, useNavigate } from "react-router-dom";
-// import { useState } from "react";
+import { useState, createContext } from "react";
 import Landing from "./routes/Landing";
 import ScrollTop from "./components/ScrollTop";
 import Invalid from "./routes/Invalid";
 import "./index.css";
 
+// we now have context for each componenet wanting to get access to flashcardDecks and flashcards
+export const FlashardContext = createContext();
+
 const ClerkRoutes = () => {
   const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
   const navigate = useNavigate();
-  // const [flashcardDecks, setFlashcardDecks] = useState([]);
-  // const [flashcards, setFlashcards] = useState([]);
+  const [flashcardDecks, setFlashcardDecks] = useState([]);
+  const [flashcards, setFlashcards] = useState([]);
 
   return (
-    <ClerkProvider publishableKey={clerkPubKey} navigate={(to) => navigate(to)}>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route
-          path="/sign-in/*"
-          element={<SignIn routing="path" path="/sign-in" />}
-        />
-        <Route
-          path="/sign-up/*"
-          element={<SignUp routing="path" path="/sign-up" />}
-        />
-        <Route
-          path="/flashcards"
-          element={
-            <>
-              <SignedIn>
-                <Landing />
-              </SignedIn>
-              <SignedOut>
-                <RedirectToSignIn />
-              </SignedOut>
-            </>
-          }
-        ></Route>
-        <Route path="*" element={<Invalid />}></Route>
-      </Routes>
-    </ClerkProvider>
+    <FlashardContext.Provider
+      value={{ flashcardDecks, setFlashcardDecks, flashcards, setFlashcards }}
+    >
+      <ClerkProvider
+        publishableKey={clerkPubKey}
+        navigate={(to) => navigate(to)}
+      >
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route
+            path="/sign-in/*"
+            element={<SignIn routing="path" path="/sign-in" />}
+          />
+          <Route
+            path="/sign-up/*"
+            element={<SignUp routing="path" path="/sign-up" />}
+          />
+          <Route
+            path="/flashcards"
+            element={
+              <>
+                <SignedIn>
+                  <Landing />
+                </SignedIn>
+                <SignedOut>
+                  <RedirectToSignIn />
+                </SignedOut>
+              </>
+            }
+          ></Route>
+          <Route path="*" element={<Invalid />}></Route>
+        </Routes>
+      </ClerkProvider>
+    </FlashardContext.Provider>
   );
 };
 
--- frontend/src/components/Navbars.jsx
+++ frontend/src/components/Navbars.jsx
@@ -1,6 +1,7 @@
 // a component which will conditionally render our Navbar as to not require 3 different components
 import { Link } from "react-router-dom";
 import { UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { FaPlus } from "react-icons/fa";
 
 const Navbars = ({ page }) => {
   if (page === "landing") {
@@ -36,7 +37,21 @@ const Navbars = ({ page }) => {
     );
   }
 
-  return <div>Navbars</div>;
+  if (page == "flashcards") {
+    <div className="navbar glass top-0 fixed z-50 bg-transparent">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-xl text-white">
+          Quizify
+        </Link>
+        <button>
+          <FaPlus/> Add
+        </button>
+      </div>
+      <div className="flex-2 mr-2">
+        <UserButton />
+      </div>
+    </div>;
+  }
 };
 
 export default Navbars;
