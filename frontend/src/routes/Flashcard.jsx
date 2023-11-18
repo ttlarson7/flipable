77a0a735eeb66f92bc9cdbc73e449fc1532f4402
Peter Mora-Stevens <petermora-stevens@Peters-MacBook-Air.local>
2023-11-17 19:48:35 -0800

    inverted title colors to test

--- frontend/src/routes/Flashcard.jsx
+++ frontend/src/routes/Flashcard.jsx
@@ -10,8 +10,8 @@ const Flashcard = ({ term, definition }) => {
       <label className="swap swap-flip text-9xl ">
         <input type="checkbox" />
         <div className="card items-center text-center swap-off bg-base-100 h-32 flex justify-center border border-primary">
-          <div className = "bg-transparent border border-primary rounded">
-            <p className="card-title text-primary font-bold text-2xl ml-3 mr-3 mb-1 ">{term}</p>
+          <div className = "bg-transparent border border-primary rounded bg-primary">
+            <p className="card-title text-base-100 font-bold text-2xl ml-3 mr-3 mb-1 ">{term}</p>
           </div>
         </div>
         <div className="card items-center text-center swap-on bg-base-100 h-32 flex justify-center border border-primary overflow-auto">
