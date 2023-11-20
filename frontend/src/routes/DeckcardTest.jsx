49edc04ec34af680c70eb56a49b2f9c6c6b74eb8
Peter Mora-Stevens <petermorastevens@gmail.com>
2023-11-19 22:04:39 -0800

    added styling to test cards

--- frontend/src/routes/DeckcardTest.jsx
+++ frontend/src/routes/DeckcardTest.jsx
@@ -1,61 +1,68 @@
-import React from "react";
+import { CiCircleCheck, CiCircleRemove } from "react-icons/ci";
 
 const DeckcardTest = ({ term, correct, index, setAnswers, answers }) => {
-
-
-    const handleAnswer = (e) => {
-        setAnswers(() => {
-            const newAnswers = [...answers];
-            newAnswers[index] = e.target.value;
-            return newAnswers;
-        })
-        console.log(answers);
-    }
-    if (correct[index] === -1) {
-        return (
-            <div className="card w-96 bg-base-100 shadow-xl">
-            <div className="card-body">
-                    <p>What is the definition of <b>{term}?</b></p>
-                <div className="card-actions justify-center">
-                    <textarea className="textarea textarea-secondary w-full" placeholder="Answer" onChange={handleAnswer}></textarea>
-                </div>
-            </div>
+  const handleAnswer = (e) => {
+    setAnswers(() => {
+      const newAnswers = [...answers];
+      newAnswers[index] = e.target.value;
+      return newAnswers;
+    });
+    console.log(answers);
+  };
+  if (correct[index] === -1) {
+    return (
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <p>
+            What is the definition of <b>{term}?</b>
+          </p>
+          <div className="card-actions justify-center">
+            <textarea
+              className="textarea textarea-secondary w-full"
+              placeholder="Answer"
+              onChange={handleAnswer}
+            ></textarea>
+          </div>
         </div>
-        )
-    }
+      </div>
+    );
+  }
 
-    if (correct[index] === 0) {
-        return (
-            <div className="indicator">
-                <span className="indicator-item badge bg-error text-black">X</span> 
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <div className="card-body border border-error rounded-2xl">
-                            <p>What is the definition of {term}?</p>
-                        <div className="card-actions justify-end border-error">
-                                <p>{answers[index]}</p>
-                        </div>
-                    </div>
-                </div>
-    </div>
-            
-        )
-    }
-
-
-    if (correct[index] === 1) {
-        return (
-            <div className="card w-96 bg-base-100 shadow-xl border-green-500">
-            <div className="card-body">
-                    <p>What is the definition of {term}?</p>
-                <div className="card-actions justify-end">
-                        <p className="border-green-500">{answers[index]}</p>
-                </div>
+  if (correct[index] === 0) {
+    return (
+      <div className="indicator">
+        <span className="indicator-item badge badge-error text-black">
+          <CiCircleRemove />
+        </span>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card-body border border-error rounded-2xl">
+            <p>What is the definition of {term}?</p>
+            <div className="card-actions justify-end border-error">
+              <p>{answers[index]}</p>
             </div>
+          </div>
         </div>
-        )
-    }
+      </div>
+    );
+  }
 
-    
-}
+  if (correct[index] === 1) {
+    return (
+      <div className="indicator">
+        <div className="card w-96 bg-base-100 shadow-xl border border-success">
+          <span className="indicator-item badge badge-success text-black">
+            <CiCircleCheck />
+          </span>
+          <div className="card-body">
+            <p>What is the definition of {term}?</p>
+            <div className="card-actions justify-end">
+              <p className="border-success">{answers[index]}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default DeckcardTest;
--- frontend/src/routes/Decktest.jsx
+++ frontend/src/routes/Decktest.jsx
@@ -1,42 +1,40 @@
-import Navbars from '../components/Navbars'
-import Footer from '../components/Footer';
+import Navbars from "../components/Navbars";
+import Footer from "../components/Footer";
 import { FlashcardContext } from "../App";
 import { useContext } from "react";
 import { useState } from "react";
-import DeckcardTest from './DeckcardTest';
+import DeckcardTest from "./DeckcardTest";
 const Decktest = () => {
-    const { flashCards } = useContext(FlashcardContext);
-    const initialAnswers = Array.from({ length: flashCards.length }, () => "");
-    const [answers, setAnswers] = useState(initialAnswers);
-    const [correct, setCorrect] = useState(Array.from({ length: flashCards.length }, () => -1))
-    
+  const { flashCards } = useContext(FlashcardContext);
+  const initialAnswers = Array.from({ length: flashCards.length }, () => "");
+  const [answers, setAnswers] = useState(initialAnswers);
+  const [correct, setCorrect] = useState(
+    Array.from({ length: flashCards.length }, () => -1)
+  );
 
-    
-    
-    return (
+  return (
     <div className="bg-neutral">
       <Navbars page="test"></Navbars>
 
       <div className="mt-24 bg-neutral"></div>
       <div className=" min-h-screen flex justify-center">
         <ul className="grid lg:grid-cols-1 sm:grid-cols-1 gap-4 m-4">
-        {flashCards.map((card, i) => (
+          {flashCards.map((card, i) => (
             <DeckcardTest
               key={i}
-                index={i}
-                term={card.term}
-                correct={correct}
-                answers={answers}
-                setAnswers={setAnswers}
+              index={i}
+              term={card.term}
+              correct={correct}
+              answers={answers}
+              setAnswers={setAnswers}
             />
           ))}
-          
         </ul>
       </div>
 
       <Footer></Footer>
     </div>
-    )
-}
+  );
+};
 
 export default Decktest;
