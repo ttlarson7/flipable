141db41c596e164cb055a4a42f18a888efa0a28e
Danielle Chang <changda@oregonstate.edu>
2023-11-28 17:24:20 -0800

    moved post stuff into helper functions

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,10 @@ async function main() {
     //await mongoose.connect('mongodb://Quizzers:I<3Quizify@127.0.0.1:27017/test');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+
+  addUser("23rjklsd908f0s");
+  addDeck("CS 261", "Computer Science", "Data structures flashcards", 0, "23rjklsd908f0s");
+  addCard("Array", "Collection of items of same data type stored at contiguous memory locations", 0, "23rjklsd908f0s");
 }
 
 const Schema = mongoose.Schema;
@@ -91,84 +95,157 @@ const User = mongoose.model("User", userSchema);
 const Deck = mongoose.model("Deck", deckSchema);
 const Card = mongoose.model("Card", cardSchema);
 
-//add new user
-app.post('/newuser', async(req, res) => {
+function addUser(newId){
     try{
-        const newUser = await User.create({
-            userId: req.body.userId,
+        const newUser = User.create({
+            userId: newId,
             decks: []
         });
         //add user to mongodb database
         newUser.save();
-        res.send("successfully created new user");
+        console.log("success");
     } catch{
-        res.send("error");
+        console.log("error");
     }
-});
+}
 
-//add new deck
-app.post('/newdeck', async(req, res) => {
+//add new user
+// app.post('/newuser', async(req, res) => {
+//     try{
+//         const newUser = await User.create({
+//             userId: req.body.userId,
+//             decks: []
+//         });
+//         //add user to mongodb database
+//         newUser.save();
+//         res.send("successfully created new user");
+//     } catch{
+//         res.send("error");
+//     }
+// });
+
+function addDeck(deckName, deckCategory, deckDesc, i, parentId){
     try{
-        const newDeck = await Deck.create({
-            name: req.body.deckName,
-            category: req.body.deckCategory,
-            description: req.body.deckDesc,
+        const newDeck = Deck.create({
+            name: deckName,
+            category: deckCategory,
+            description: deckDesc,
             cards: [],
-            index: req.body.i,
-            parentUser: req.body.parentUser
+            index: i,
+            parentUser: parentId
         });
-        console.log(newDeck);
         //add deck to current user's deck array, update mongoDB
-        const currentUser = User.findOne({userId: parentUser});
+        const currentUser = User.findOne({userId: parentId});
         currentUser.decks.push(newDeck);
         currentUser.save();
-        res.send("successfully created new deck");
+        console.log("success");
     } catch{
-        res.send("error");
+        console.log("error");
     }
+}
 
-});
+//add new deck
+// app.post('/newdeck', async(req, res) => {
+//     try{
+//         const newDeck = await Deck.create({
+//             name: req.body.deckName,
+//             category: req.body.deckCategory,
+//             description: req.body.deckDesc,
+//             cards: [],
+//             index: req.body.i,
+//             parentUser: req.body.parentUser
+//         });
+//         console.log(newDeck);
+//         //add deck to current user's deck array, update mongoDB
+//         const currentUser = User.findOne({userId: parentUser});
+//         currentUser.decks.push(newDeck);
+//         currentUser.save();
+//         res.send("successfully created new deck");
+//     } catch{
+//         res.send("error");
+//     }
 
-//add new card to deck
-app.post('/newcard', async(req, res) => {
+// });
+
+function addCard(cardTerm, cardDef, cardDeck, cardUser){
     try{
-        const newCard = await Card.create({
-            term: req.body.term,
-            definition: req.body.definition,
-            parentDeck: req.body.parentDeck,
-            parentUser: req.body.parentUser
+        const newCard = Card.create({
+            term: cardTerm,
+            definition: cardDef,
+            parentDeck: cardDeck,
+            parentUser: cardUser
         });
         //add card to current user's deck, update mongoDB
-        const currentUser = User.findOne({userId: parentUser});
-        currentUser.decks[parentDeck].push(newCard);
+        const currentUser = User.findOne({userId: cardUser});
+        currentUser.decks[cardDeck].push(newCard);
         currentUser.save();
-        res.send("successfully created new card");
+        console.log("success");
     } catch{
-        res.send("error");
+        console.log("error");
     }
-});
+}
+
+//add new card to deck
+// app.post('/newcard', async(req, res) => {
+//     try{
+//         const newCard = await Card.create({
+//             term: req.body.term,
+//             definition: req.body.definition,
+//             parentDeck: req.body.parentDeck,
+//             parentUser: req.body.parentUser
+//         });
+//         //add card to current user's deck, update mongoDB
+//         const currentUser = User.findOne({userId: parentUser});
+//         currentUser.decks[parentDeck].push(newCard);
+//         currentUser.save();
+//         res.send("successfully created new card");
+//     } catch{
+//         res.send("error");
+//     }
+// });
+
+function removeDeck(parentId, deckIndex){
+    try{
+        const currentUser = User.findOne({userId: parentId});
+        currentUser.decks.splice(deckIndex, 1);
+        currentUser.save();
+        console.log("success");
+    } catch{
+        console.log("error");
+    }
+}
 
 //remove deck, pass in parent user ID and index of deck to use
-app.post('/removedeck', async(req, res) => {
+// app.post('/removedeck', async(req, res) => {
+//     try{
+//         const currentUser = User.findOne({userId: req.body.parentUser});
+//         currentUser.decks.splice(req.body.i, 1);
+//         currentUser.save();
+//     } catch{
+//         res.send("error");
+//     }
+// });
+
+function removeCard(parentId, deckIndex, cardIndex){
     try{
-        const currentUser = User.findOne({userId: req.body.parentUser});
-        currentUser.decks.splice(req.body.i, 1);
+        const currentUser = User.findOne({userId: parentId});
+        currentUser.decks[deckIndex].splice(cardIndex, 1);
         currentUser.save();
     } catch{
-        res.send("error");
+        console.log("error");
     }
-});
+}
 
 //remove card from deck, pass in parent user ID, index of deck card is in (i), and index of card (j)
-app.post('/removecard', async(req, res) => {
-    try{
-        const currentUser = User.findOne({userId: req.body.parentUser});
-        currentUser.decks[res.body.i].splice(res.body.j, 1);
-        currentUser.save();
-    } catch{
-        res.send("error");
-    }
-});
+// app.post('/removecard', async(req, res) => {
+//     try{
+//         const currentUser = User.findOne({userId: req.body.parentUser});
+//         currentUser.decks[res.body.i].splice(res.body.j, 1);
+//         currentUser.save();
+//     } catch{
+//         res.send("error");
+//     }
+// });
 
 
 
@@ -177,6 +254,10 @@ app.post('/removecard', async(req, res) => {
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
+    addUser("23rjklsd908f0s");
+    addDeck("CS 261", "Computer Science", "Data structures flashcards", 0, "23rjklsd908f0s");
+    addCard("Array", "Collection of items of same data type stored at contiguous memory locations", 0, "23rjklsd908f0s");
+  
 })
 
 
